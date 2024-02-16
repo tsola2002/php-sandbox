c0768edac5c474b035218c9d8c7059376e913bc7
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2024-02-16 13:28:39 +0100

    added more regular expression functions

--- 07_files/regex.php
+++ 07_files/regex.php
@@ -9,23 +9,70 @@
     <h1>Regular Expressions</h1>
 
     <?php 
+        // 1. search our string to see if we have certain characters
+        $subject = "My name is shola. shola is my name";
+        $numbers = "1244456789";
+        $word = "shape";
+        $email = "@";
 
-    // sample string
-    $string = "My name is shola, shola is my name";
-    
-    // 1. searches our string to see if we have certain characters
-    //if(preg_match("/shola/", $string, $array)){
-    //      if(preg_match_all("/shola/", $string, $array)){
-    //    echo "Success: it is a match" . "<br/>";
-    //  print_r($array);
-    // } else {
-    //    echo "Failure: it is not a match";
-    //}
 
-    // 2. using preg_replace to preg_replace certain strings
-    //$replaced = preg_replace("/shola/","John", $string);
-    //echo $replaced;
+
+       if(preg_match("/john/", $subject)){
+            echo "Success: it is match" . "<br>";
+       } else {
+            echo "Failure: it is not a match" . "<br>";
+       }
+
+       if(preg_match("/^shp/", $word)){
+        echo "Success: numbers is a match" . "<br>";
+        } else {
+                echo "Failure: numbers is not a match" . "<br>";
+        }
+
+        if(preg_match("/^shp/", $word)){
+            echo "Success: numbers is a match" . "<br>";
+            } else {
+                    echo "Failure: numbers is not a match" . "<br>";
+            }
+
+            if(preg_match("/s+/", $word)){
+                echo "Success: s is a match" . "<br>";
+                } else {
+                        echo "Failure: s is not a match" . "<br>";
+                }  
+            
+                if(preg_match("/s./", $word)){
+                    echo "Success: s is a match" . "<br>";
+                    } else {
+                            echo "Failure: s is not a match" . "<br>";
+                    }  
+            
+                    if(preg_match("/q|z/", $word)){
+                        echo "Success: or is a match" . "<br>";
+                        } else 
+                    {
+                                echo "Failure: or is not a match" . "<br>";
+                    }
+                    
+                    if(preg_match("/\W\D/", $email)){
+                        echo "Success: non-word character is a match" . "<br>";
+                        } else {
+                                echo "Failure: non-word character is not a match" . "<br>";
+                        }  
+
+                        if(preg_match("/4{3}/", $numbers)){
+                            echo "Success: occurence is a match" . "<br>";
+                            } else {
+                                    echo "Failure: occurence character is not a match" . "<br>";
+                            }          
+        
+            // 2. using preg_replace to preg_replace to replace certain strings
+            $new_string = "John";
+            $replaced = preg_replace("/shola/", $new_string, $subject);
+            echo $replaced . "<br>";
+        
 
     ?>
+    
 </body>
 </html>
