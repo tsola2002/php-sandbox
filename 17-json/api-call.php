42996be4107e2e2fe677d30404b6a67b5d10c7c3
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2024-04-18 11:55:15 +0100

    triggered an api call

---
+++ 17-json/api-call.php
@@ -0,0 +1,23 @@
+<?php
+
+$response = file_get_contents("https://randomuser.me/api");
+
+$data = json_decode($response, true);
+
+$arr = array('Name' => 'Omatsola', 'Age' => 30, 'Address' => 'Lekki');
+
+$encoded_data = json_encode($arr);
+
+//var_dump($encoded_data);
+
+//var_dump($data);
+
+echo $data['results'][0]['location']['coordinates']['longitude'];
+
+
+// echo $data;
+
+
+//print_r($data);
+
+?>
